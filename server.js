a58f68fc67d1ba45855eb853348e62ce288a63a8
gurpreet1426 <gurpreetaws@gmail.com>
2025-05-14 22:47:36 +0530

    add new endpoint

--- server.js
+++ server.js
@@ -3,6 +3,9 @@
     app.get('/api/get',(req,res)=>{
         res.send({message:'nodejs aws deployement'})
     })
+    app.get('/api/get/users',(req,res)=>{
+        res.send({name:'Dev',email:'test@gmail.com','role':'developer'})
+    })
     app.listen(9000,(req,res)=>{
         console.log('server running on pot 9000')
     })
